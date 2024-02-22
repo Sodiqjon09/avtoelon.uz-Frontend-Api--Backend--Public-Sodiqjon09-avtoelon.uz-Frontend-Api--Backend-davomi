289158626712a1abd09e8f384cff3bdce475c144
Sodiqjon Sharipov <134689903+Sodiqjon09@users.noreply.github.com>
2024-02-22 21:02:22 +0500

    full

--- src/client/components/ContentQoyida/ContentQoyida.jsx
+++
@@ -1,8 +0,0 @@
-import './ContentQoyida.css'
-
-function ContentQoyida() {
-  return <div id="contentQoyida">
-    <h1>E`lonlarni joylashtirish qoidalari</h1>
-  </div>;
-}
-export default ContentQoyida;
